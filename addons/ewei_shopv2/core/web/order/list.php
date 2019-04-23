3f23f4d6a0d820e363f926443cb55a80448ee998
吴继 <wuji@MacBook-Pro.local>
2019-04-23 15:52:56 +0800

    内购添加积分功能

--- addons/ewei_shopv2/core/web/order/list.php
+++ addons/ewei_shopv2/core/web/order/list.php
@@ -909,9 +909,12 @@ class List_EweiShopV2Page extends WebPage
 		$good_id = $_GPC['good_id'];
 		// 进货数量
 		$number = $_GPC['number'];
+		// 价钱
+		$money = $_GPC['money'];
 		if (empty($order_id) || empty($shop_id) || empty($good_id) || empty($number)){
 			exit(json_encode(['status' => 0, 'msg' => '操作异常!!!']));
 		}
+		$store = pdo_get('ewei_shop_store', ['id' => $shop_id], 'uid');
 		if ($role_id == 3) {
 			$res = pdo_update('ewei_shop_purchase_order',
 				['order_status' => 1],
@@ -921,11 +924,12 @@ class List_EweiShopV2Page extends WebPage
 			exit(json_encode(['status' => 1, 'msg' => '确认收款成功']));
 		}
 		if ($role_id == 4) {
+			$update_user = pdo_update('users', ['integral' => number_format($money) * -100], ['uid' => $store['uid']]);
 			$res = pdo_update('ewei_shop_purchase_order',
 				['order_status' => 2],
 				['id' => $order_id]
 			);
-			if (!$res) exit(json_encode(['status' => 0, 'msg' => '确认发货失败']));
+			if (!$res || !$update_user) exit(json_encode(['status' => 0, 'msg' => '确认发货失败']));
 			exit(json_encode(['status' => 1, 'msg' => '确认发货成功']));
 		}
 		if ($role_id == 0) {
@@ -938,11 +942,12 @@ class List_EweiShopV2Page extends WebPage
 				if (!$res) exit(json_encode(['status' => 0, 'msg' => '确认收款失败']));
 				exit(json_encode(['status' => 1, 'msg' => '确认收款成功']));
 			} else if ($order_status['order_status'] == 1) {
+				$update_user = pdo_update('users', ['integral' => number_format($money) * -100], ['uid' => $store['uid']]);
 				$res = pdo_update('ewei_shop_purchase_order',
 					['order_status' => 2],
 					['id' => $order_id]
 				);
-				if (!$res) exit(json_encode(['status' => 0, 'msg' => '确认收款失败']));
+				if (!$res || !$update_user) exit(json_encode(['status' => 0, 'msg' => '确认收款失败']));
 				exit(json_encode(['status' => 1, 'msg' => '确认收款成功']));
 			}
 		}
--- addons/ewei_shopv2/core/web/purchase/index.php
+++ addons/ewei_shopv2/core/web/purchase/index.php
@@ -46,7 +46,6 @@ class Index_EweiShopV2Page extends WebPage
 		foreach ($data as $v) {
 			$res = pdo_insert('ewei_shop_purchase_order', $v);
 		}
-		
 		if (!$res) $this->message('进货失败', webUrl('purchase/add'));
 		$this->message('进货成功', webUrl('purchase'));
 	}
